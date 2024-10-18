3f09a98cfee9cd13d95e1492d391ef1f907b0aa8
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-10-17 18:07:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -47,4 +47,15 @@ window.onload = function() {
         cellsContainer.appendChild(cell);
     }
     updateAcceptanceRate();
+
+    // Регистрация сервис-воркера
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+            console.error('Service Worker registration failed:', error);
+        });
+    }
 };
