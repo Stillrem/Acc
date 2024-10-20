9a9843b9d16a37eb1f64c4bb2b3dcfaf2aec8edd
Stillrem <106412413+Stillrem@users.noreply.github.com>
2024-10-20 03:21:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@
 
             function toggleLock() {
                 isLocked = !isLocked;
-                localStorage.setItem('isLocked', isLocked.toString());
+                localStorage.setItem('isLocked', JSON.stringif(isLocked));
 
                 const cells = document.querySelectorAll('.cell');
                 cells.forEach((cell, index) => {
